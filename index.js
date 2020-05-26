c9d6e3a62501573f90c4492bc5fb2ee4943b9056
TonyChouteau <tony.chouteau@outlook.fr>
2020-05-27 00:02:42 +0200

    add resolving AI

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 //====================
 // Define
 
-let WIDTH = 50;
-let HEIGHT = 50;
+// Matrix
+const WIDTH = 50;
+const HEIGHT = 50;
 
-let RANDTODIG = 0.2;
-let SPEED = RANDTODIG>0.5?10:20
+// Random & Speed
+const RAND_DIG = 0.8;
+const RAND_KEEP = 0.9;
+const SPEED = 20;
 
+// States
+const GENERATING = 0,RESOLVING = 1, END = 2, ERROR = 3;
+let state = GENERATING;
+
+// To generate
 let board = [];
-
 let current = [];
+
+// To init
+let start, end;
+
+// To resolve 
+let previous = [];
+let closed = [];
+let player;
 
 //====================
 // Utils
@@ -41,8 +56,10 @@ function downloadObjectAsText(exportObj, exportName){
 // Setup
 
 function setup(){
-	createCanvas(900, 900);
+	let canvas = createCanvas(900, 900);
+	canvas.parent("canvas-container");
 	frameRate(SPEED);
+	noStroke();
 
 	for (let i=0; i<HEIGHT; i++){
 		let line = [];
@@ -161,44 +178,174 @@ function digUp(coords){
 	x = coords.x;
 	y = coords.y;
 
-	board[y-1][x] = 1
+	board[y-1][x] = 1;
 
 	return {
 		"x": x,
 		"y": y-1
-	}
+	};
 }
 function digDown(coords){
 	x = coords.x;
 	y = coords.y;
 
-	board[y+1][x] = 1
+	board[y+1][x] = 1;
 
 	return {
 		"x": x,
 		"y": y+1
-	}
+	};
 }
 function digLeft(coords){
 	x = coords.x;
 	y = coords.y;
 
-	board[y][x-1] = 1
+	board[y][x-1] = 1;
 
 	return {
 		"x": x-1,
 		"y": y
-	}
+	};
 }
 function digRight(coords){
 	x = coords.x;
 	y = coords.y;
 
-	board[y][x+1] = 1
+	board[y][x+1] = 1;
 
 	return {
 		"x": x+1,
 		"y": y
+	};
+}
+
+//====================
+// Draw sub functions
+
+function generate(){
+
+	let next = [];
+	let currentIter;
+	let cell;
+
+	for (let i in current) {
+
+		currentIter = current[i];
+		let keep = randInt(0,4);
+
+		if (keep == 0 && checkUp(currentIter) && Math.random()<RAND_DIG){
+				cell = digUp(currentIter);
+				next.push(cell);
+				if (Math.random())
+				next.push(currentIter);
+		} else if (keep == 1 && checkDown(currentIter) && Math.random()<RAND_DIG){
+				cell = digDown(currentIter);
+				next.push(cell);
+				next.push(currentIter);
+		} else if (keep == 2 && checkRight(currentIter) && Math.random()<RAND_DIG){
+				cell = digRight(currentIter);
+				next.push(cell);
+				next.push(currentIter);
+		} else if (keep == 3 && checkLeft(currentIter) && Math.random()<RAND_DIG){
+				cell = digLeft(currentIter);
+				next.push(cell);
+				next.push(currentIter);
+		} else {
+			if (checkUp(currentIter) || checkDown(currentIter) || checkLeft(currentIter) || checkRight(currentIter)) {
+				next.push(currentIter);
+			}
+		}
+	}
+
+	current = next;
+
+	if (current.length == 0){
+		console.log("Done");
+		state = RESOLVING;
+		frameRate(SPEED*5);
+		//downloadObjectAsJson(board, "export");
+		//downloadObjectAsText("fafzagaazg", "export");
+	}
+}
+
+function setPoints(){
+	let startCell, endCell;
+
+	let i=0;
+	while (i < HEIGHT && (startCell == undefined || endCell == undefined)){
+		let j=0
+		while (j < WIDTH && (startCell == undefined || endCell == undefined)){
+			if (startCell == undefined && board[i][j] == 1){
+				startCell = {
+					"x": j,
+					"y": i
+				};
+			}
+			if (endCell == undefined && board[HEIGHT-1-i][WIDTH-1-j] == 1){
+				endCell = {
+					"x": WIDTH-1-j,
+					"y": HEIGHT-1-i
+				};
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return [startCell, endCell];
+}
+
+function alreadyGone(list, x, y){
+	let previousFiltered = list.filter((value) => {
+		if (value.x == x && value.y == y){
+			return true;
+		} else {
+			return false;
+		}
+	})
+
+	return previousFiltered.length > 0
+}
+
+function resolve(){
+	if (!alreadyGone(previous, player.x, player.y)){
+		previous.push({
+			"x": player.x,
+			"y": player.y
+		});
+		board[player.y][player.x] = 2;
+	}
+
+	if (board[player.y-1][player.x] != 0 && !alreadyGone(previous, player.x, player.y-1)) {
+		player.y -= 1
+	} else if (board[player.y+1][player.x] != 0 && !alreadyGone(previous, player.x, player.y+1)) {
+		player.y += 1
+	} else if (board[player.y][player.x-1] != 0 && !alreadyGone(previous, player.x-1, player.y)) {
+		player.x -= 1
+	} else if (board[player.y][player.x+1] != 0 && !alreadyGone(previous, player.x+1, player.y)) {
+		player.x += 1
+	} else {
+		closed.push({
+			"x": player.x,
+			"y": player.y
+		});
+		board[player.y][player.x] = 3;
+		if (board[player.y-1][player.x] != 0 && !alreadyGone(closed, player.x, player.y-1)) {
+			player.y -= 1
+		} else if (board[player.y+1][player.x] != 0 && !alreadyGone(closed, player.x, player.y+1)) {
+			player.y += 1
+		} else if (board[player.y][player.x-1] != 0 && !alreadyGone(closed, player.x-1, player.y)) {
+			player.x -= 1
+		} else if (board[player.y][player.x+1] != 0 && !alreadyGone(closed, player.x+1, player.y)) {
+			player.x += 1
+		} else {
+			console.log("ERROR")
+			state = ERROR;
+		}
+	}
+
+	if (player.x == end.x && player.y == end.y){
+		state = END;
 	}
 }
 
@@ -210,59 +357,41 @@ function draw(){
 		for (let j=0; j<WIDTH; j++){
 			if (board[i][j] == 1){
 				fill(255);
-				noStroke();
+				rect(j*width/WIDTH, i*height/HEIGHT, width/WIDTH, height/HEIGHT);
+			}
+			if (board[i][j] == 2){
+				fill(255, 200, 0);
+				rect(j*width/WIDTH, i*height/HEIGHT, width/WIDTH, height/HEIGHT);
+			}
+			if (board[i][j] == 3){
+				fill(255, 150, 0);
 				rect(j*width/WIDTH, i*height/HEIGHT, width/WIDTH, height/HEIGHT);
 			}
 		}
 	}
+	
+	if (state == GENERATING){
+		generate();
 
-	let next = [];
-	let currentIter;
-	let cell;
-
-	for (let i in current) {
-		currentIter = current[i];
-		if (checkUp(currentIter)){
-			if (Math.random()<RANDTODIG){
-				cell = digUp(currentIter);
-				next.push(cell);
-			} else {
-				next.push(currentIter);
-			}
-		}
-		if (checkDown(currentIter)){
-			if (Math.random()<RANDTODIG){
-				cell = digDown(currentIter);
-				next.push(cell);
-			} else {
-				next.push(currentIter);
-			}
-		}
-		if (checkRight(currentIter)){
-			if (Math.random()<RANDTODIG){
-				cell = digRight(currentIter);
-				next.push(cell);
-			} else {
-				next.push(currentIter);
-			}
-		}
-		if (checkLeft(currentIter)){
-			if (Math.random()<RANDTODIG){
-				cell = digLeft(currentIter);
-				next.push(cell);
-			} else {
-				next.push(currentIter);
-			}
+		// Init to resolve
+		if (state != GENERATING){
+			[start, end] = setPoints();
+			player = start;
 		}
 	}
+	else if (state == RESOLVING) {
+		fill(255, 0, 0);
+		rect(player.x*width/WIDTH, player.y*height/HEIGHT, width/WIDTH, height/HEIGHT);
+		fill(0, 255, 0);
+		rect(end.x*width/WIDTH, end.y*height/HEIGHT, width/WIDTH, height/HEIGHT);
 
-	current = next;
-
-	if (current.length == 0){
-		console.log("Done");
+		resolve();
+	} 
+	else if (state == END){
 		noLoop();
-
-		//downloadObjectAsJson(board, "export");
-		//downloadObjectAsText("fafzagaazg", "export");
+		fill(255, 0, 0);
+		rect(player.x*width/WIDTH, player.y*height/HEIGHT, width/WIDTH, height/HEIGHT);
+		fill(0, 255, 0);
+		rect(end.x*width/WIDTH, end.y*height/HEIGHT, width/WIDTH, height/HEIGHT);
 	}
 }
